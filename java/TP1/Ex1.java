3b0a1f2f8a0bf88b2e4cd01d92653642536ab65d
Seng Enghav <108415579+Calbil09@users.noreply.github.com>
2023-03-25 08:57:54 +0700

    Add files via upload

---
+++ java/TP1/Ex1.java
@@ -0,0 +1,8 @@
+package s2java.src.TP1;
+public class Ex1 {
+    public static void main(String[] args) {
+        String name ="EngHav";
+        System.out.println("Hello "+name+"!");
+    }
+    
+}
---
+++ java/TP1/Ex2.java
@@ -0,0 +1,18 @@
+package s2java.src.TP1;
+public class Ex2 {
+    public static void main(String[] args) {
+        System.out.println(
+                  "\\n                          Line break. \n"
+                + "\\t                          Tabulation. \n"
+                + "\\\"                       Single Quote. \n"
+                + "\\\"                       Double Quote. \n"
+                + "\\\\                           \\  Sign. \n"
+                + "\\\\\\\\                     \\\\  Sign. \n"
+                + "//                         Line Comment. \n"
+                + "/*....*/                 Block  Comment. \n"
+                + "\"\"\"                                   \n"
+                + "                             Text Block. \n"
+                + "\"\"\"  ");
+    }
+    
+}
